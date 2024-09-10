23360ef1db7aa9bb568f1faa57a385231ca8174a
Henrique Goncalves <kamus@hadenes.io>
2024-09-10 03:55:18 -0300

    feat: Add script to display current IP address or VPN status

---
+++ home/macos/resources/sketchybar/plugins/stats/scripts/ip_address.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source "$HOME/.config/sketchybar/colors.sh"
+source "$HOME/.config/sketchybar/userconfig.sh"
+
+IP_ADDRESS=$(scutil --nwi | grep address | sed 's/.*://' | tr -d ' ' | head -1)
+IS_VPN=$(scutil --nwi | grep -m1 'utun' | awk '{ print $1 }')
+
+if [[ $IS_VPN != "" ]]; then
+    COLOR=$CYAN
+    ICON=
+    LABEL="VPN"
+elif [[ $IP_ADDRESS != "" ]]; then
+    COLOR=$BLUE
+    ICON=
+    LABEL=$IP_ADDRESS
+else
+    COLOR=$WHITE
+    ICON=
+    LABEL="Not Connected"
+fi
+
+sketchybar --set $NAME icon.color=$COLOR label.color=$COLOR \
+    icon=$ICON \
+    label="$LABEL"
