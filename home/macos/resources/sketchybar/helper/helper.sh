92b3a6b718d312e5ebf0c1b90488f5d5a1f47092
Henrique Goncalves <kamus@hadenes.io>
2024-09-11 22:45:28 -0300

    refactor: update helper script and makefile paths

--- home/macos/resources/sketchybar/helper/helper.sh
+++ home/macos/resources/sketchybar/helper/helper.sh
@@ -3,4 +3,4 @@
 HELPER=git.felix.helper
 killall helper
 cd "$HOME"/.config/sketchybar/helper && make
-"$HOME"/.config/sketchybar/helper/helper "$HELPER" >/dev/null 2>&1 &
+"/tmp/.$(whoami)_sketchybar_helper" "$HELPER" >/dev/null 2>&1 &
