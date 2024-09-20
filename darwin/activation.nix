374c0064eab7ae7ccac1c5e95bc5bb8a71f6f7fa
Henrique Goncalves <kamus@hadenes.io>
2024-09-20 20:42:10 -0300

    dropbox

--- darwin/activation.nix
+++ darwin/activation.nix
@@ -1,33 +1,35 @@
 { pkgs, packages, ... }:
 {
 
-  activationScripts = {
-    autoSmb = {
-      enable = true;
-      text = ''
-        source ${packages.colorScript}
-        _iNote "Enabling auto_smb"
+  system = {
+    activationScripts = {
+      autoSmb = {
+        enable = true;
+        text = ''
+          source ${packages.colorScript}
+          _iNote "Enabling auto_smb"
 
-        /usr/bin/sudo /run/current-system/sw/bin/enable_auto_smb
-      '';
-    };
+          /usr/bin/sudo /run/current-system/sw/bin/enable_auto_smb
+        '';
+      };
 
-    postUserActivation = {
-      text = ''
-        source ${packages.colorScript}
-        _iNote "Activating settings"
+      postUserActivation = {
+        text = ''
+          source ${packages.colorScript}
+          _iNote "Activating settings"
 
-        /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
-      '';
-    };
+          /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+        '';
+      };
 
-    restartDock = {
-      text = ''
-        source ${packages.colorScript}
-        _iNote "Restarting Dock"
+      restartDock = {
+        text = ''
+          source ${packages.colorScript}
+          _iNote "Restarting Dock"
 
-        /usr/bin/killall Dock
-      '';
+          /usr/bin/killall Dock
+        '';
+      };
     };
   };
 }
