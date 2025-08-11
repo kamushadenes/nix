e414f5d02ea22b7b2ed8ed42b5d2cbe270efc004
Henrique Goncalves <kamus@hadenes.io>
2025-08-11 10:35:52 -0300

    .

--- home_other.nix
+++ home_other.nix
@@ -21,6 +21,17 @@
       enable = true;
       enableCompletion = true;
       enableVteIntegration = true;
+      bashrcExtra = ''
+        export PATH=$PATH:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin
+
+        export PATH=$PATH:/opt/homebrew/bin
+
+        export PATH=$PATH:/Users/yjrodrigues/.mcuxpressotools/dtc-1.6.1-macos/dtc
+
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+
+        export PATH="/opt/homebrew/opt/python/libexec/bin:$PATH"
+      '';
     };
   };
 
