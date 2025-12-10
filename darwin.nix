0a57b8a36f9cff59b86a77ba7631624096b2e5b3
Henrique Goncalves <kamus@hadenes.io>
2025-12-09 23:39:07 -0300

    fix: nix cache

--- darwin.nix
+++ darwin.nix
@@ -85,6 +85,8 @@ in
   nix.package = pkgs.lixPackageSets.stable.lix;
 
   nix.settings = {
+    connect-timeout = 5;
+    fallback = false;
     substituters = [
       "http://ncps.hyades.io:8501"
       "https://nix-community.cachix.org"
@@ -94,6 +96,9 @@ in
       "ncps.hyades.io:/02vviGNLGYhW28GFzmPFupnP6gZ4uDD4G3kRnXuutE="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+    trusted-substituters = [
+      "http://ncps.hyades.io:8501"
     ];
     trusted-users = [
       "kamushadenes"
--- nixos.nix
+++ nixos.nix
@@ -72,6 +72,8 @@ in
   nix.package = pkgs.lixPackageSets.stable.lix;
 
   nix.settings = {
+    connect-timeout = 5;
+    fallback = false;
     substituters = [
       "http://ncps.hyades.io:8501"
       "https://nix-community.cachix.org"
@@ -81,6 +83,9 @@ in
       "ncps.hyades.io:/02vviGNLGYhW28GFzmPFupnP6gZ4uDD4G3kRnXuutE="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+    trusted-substituters = [
+      "http://ncps.hyades.io:8501"
     ];
     trusted-users = [
       "kamushadenes"
