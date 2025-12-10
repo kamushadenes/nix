12e6c5e18910cece215d8009c7ea99a57380d1c6
Henrique Goncalves <kamus@hadenes.io>
2025-12-10 01:16:30 -0300

    fix: nix cache

--- nixos.nix
+++ nixos.nix
@@ -15,7 +15,7 @@ in
   imports = [
     hardware
 
-    ./shared/cachix.nix
+    ./shared/cache.nix
 
     ./nixos/audio.nix
     ./nixos/browser.nix
@@ -70,25 +70,4 @@ in
   ];
 
   nix.package = pkgs.lixPackageSets.stable.lix;
-
-  nix.settings = {
-    connect-timeout = 5;
-    fallback = false;
-    substituters = [
-      "http://ncps.hyades.io:8501"
-      "https://nix-community.cachix.org"
-      "https://cache.nixos.org"
-    ];
-    trusted-public-keys = [
-      "ncps.hyades.io:/02vviGNLGYhW28GFzmPFupnP6gZ4uDD4G3kRnXuutE="
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-    trusted-substituters = [
-      "http://ncps.hyades.io:8501"
-    ];
-    trusted-users = [
-      "kamushadenes"
-    ];
-  };
 }
