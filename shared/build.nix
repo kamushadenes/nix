00fe56805e8433cc658e10e5dcc2ac093d442de9
Henrique Goncalves <kamus@hadenes.io>
2025-12-10 01:59:40 -0300

    fix: nix distributed builds

--- shared/build.nix
+++ shared/build.nix
@@ -7,56 +7,60 @@
 }:
 
 {
-  nix.buildMachines =
-    [ ]
-    ++ lib.optionals (machine != "studio.hyades.io") [
+  nix = {
+    buildMachines =
+      [ ]
+      ++ lib.optionals (machine != "studio.hyades.io") [
+        {
+          hostName = "studio.hyades.io";
+          system = "aarch64-darwin";
+          protocol = "ssh-ng";
+          # if the builder supports building for multiple architectures,
+          # replace the previous line by, e.g.
+          # systems = ["x86_64-linux" "aarch64-linux"];
+          maxJobs = 2;
+          speedFactor = 2;
+          supportedFeatures = [
+            "big-parallel"
+          ];
+          mandatoryFeatures = [ ];
+        }
+      ]
+      ++ lib.optionals (machine != "macbook-m3-pro.hyades.io") [
 
-      {
-        hostName = "studio.hyades.io";
-        system = "aarch64-darwin";
-        protocol = "ssh-ng";
-        # if the builder supports building for multiple architectures,
-        # replace the previous line by, e.g.
-        # systems = ["x86_64-linux" "aarch64-linux"];
-        maxJobs = 2;
-        speedFactor = 2;
-        supportedFeatures = [
-          "big-parallel"
-        ];
-        mandatoryFeatures = [ ];
-      }
-    ]
-    ++ lib.optionals (machine != "macbook-m3-pro.hyades.io") [
+        {
+          hostName = "mac.hyades.io";
+          system = "aarch64-darwin";
+          protocol = "ssh-ng";
+          maxJobs = 2;
+          speedFactor = 2;
+          supportedFeatures = [
+            "big-parallel"
+          ];
+          mandatoryFeatures = [ ];
+        }
+      ]
+      ++ lib.optionals (machine != "w-henrique.hyades.io") [
 
-      {
-        hostName = "mac.hyades.io";
-        system = "aarch64-darwin";
-        protocol = "ssh-ng";
-        maxJobs = 2;
-        speedFactor = 2;
-        supportedFeatures = [
-          "big-parallel"
-        ];
-        mandatoryFeatures = [ ];
-      }
-    ]
-    ++ lib.optionals (machine != "w-henrique.hyades.io") [
+        {
+          hostName = "w-henrique.hyades.io";
+          system = "aarch64-darwin";
+          protocol = "ssh-ng";
+          maxJobs = 2;
+          speedFactor = 2;
+          supportedFeatures = [
+            "big-parallel"
+          ];
+          mandatoryFeatures = [ ];
+        }
+      ];
+    distributedBuilds = true;
+    extraOptions = ''
+      	  builders-use-substitutes = true
+      	'';
 
-      {
-        hostName = "w-henrique.hyades.io";
-        system = "aarch64-darwin";
-        protocol = "ssh-ng";
-        maxJobs = 2;
-        speedFactor = 2;
-        supportedFeatures = [
-          "big-parallel"
-        ];
-        mandatoryFeatures = [ ];
-      }
-    ];
-  nix.distributedBuilds = true;
-  # optional, useful when the builder has a faster internet connection than yours
-  nix.extraOptions = ''
-    	  builders-use-substitutes = true
-    	'';
+    optimise = {
+      automatic = true;
+    };
+  };
 }
--- shared/cache.nix
+++ shared/cache.nix
@@ -1,23 +1,37 @@
 { ... }:
 {
-  nix.settings = {
-    connect-timeout = 5;
-    fallback = true;
-    substituters = [
-      "http://ncps.hyades.io:8501"
-      "https://nix-community.cachix.org"
-      "https://cache.nixos.org"
-    ];
-    trusted-public-keys = [
-      "ncps.hyades.io:/02vviGNLGYhW28GFzmPFupnP6gZ4uDD4G3kRnXuutE="
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-    trusted-substituters = [
-      "http://ncps.hyades.io:8501"
-    ];
-    trusted-users = [
-      "kamushadenes"
-    ];
+  nix = {
+    settings = {
+      connect-timeout = 5;
+      fallback = true;
+      substituters = [
+        "http://ncps.hyades.io:8501"
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org"
+      ];
+      trusted-public-keys = [
+        "ncps.hyades.io:/02vviGNLGYhW28GFzmPFupnP6gZ4uDD4G3kRnXuutE="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+      trusted-substituters = [
+        "http://ncps.hyades.io:8501"
+      ];
+      trusted-users = [
+        "kamushadenes"
+      ];
+    };
+
+    extra-substituters =
+      [ ]
+      ++ lib.optionals (machine != "studio.hyades.io") [
+        "ssh-ng://studio.hyades.io"
+      ]
+      ++ lib.optionals (machine != "macbook-m3-pro.hyades.io") [
+        "ssh-ng://mac.hyades.io"
+      ]
+      ++ lib.optionals (machine != "w-henrique.hyades.io") [
+        "ssh-ng://w-henrique.hyades.io"
+      ];
   };
 }
